0ddbd3dcef754ce3424f3a64ecde3fb0ade2c493
Frederick Brandon Thornton <97fthornton@gmail.com>
2024-11-03 11:17:25 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,25 @@
+module bnbot
+
+go 1.23.1
+
+require (
+	github.com/disgoorg/disgo v0.18.12 // indirect
+	github.com/disgoorg/json v1.2.0 // indirect
+	github.com/disgoorg/snowflake/v2 v2.0.3 // indirect
+	github.com/golang-migrate/migrate/v4 v4.17.1 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/jackc/pgerrcode v0.0.0-20220416144525-469b46aa5efa // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.6.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/sasha-s/go-csync v0.0.0-20240107134140-fcbab37b09ad // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+	lesiw.io/plain v0.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/disgoorg/disgo"
+	"github.com/disgoorg/disgo/bot"
+	"github.com/disgoorg/disgo/discord"
+	"github.com/disgoorg/disgo/events"
+	"github.com/disgoorg/disgo/gateway"
+)
+
+func main() {
+	fmt.Println(os.Getenv("DISCORD_TOKEN")) 
+
+	client, err := disgo.New(os.Getenv("DISCORD_TOKEN"),
+		// set gateway options
+		bot.WithGatewayConfigOpts(
+			// set enabled intents
+			gateway.WithIntents(
+				gateway.IntentGuilds,
+				gateway.IntentGuildMessages,
+				gateway.IntentDirectMessages,
+			),
+		),
+		// add event listeners
+		bot.WithEventListenerFunc(func(e *events.GuildVoiceJoin) {
+			err := VoiceRoleAdjustment(true, e.GenericGuildVoiceState)
+			if err != nil {
+				log.Printf("Failed to add member role: %s", err)
+			}
+		}),
+		bot.WithEventListenerFunc(func(e *events.GuildVoiceLeave) {
+			err := VoiceRoleAdjustment(false, e.GenericGuildVoiceState)
+			if err != nil {
+				log.Printf("Failed to remove member role: %s", err)
+			}
+		}),
+	)
+	
+	if err != nil {
+		panic(err)
+	}
+	// connect to the gateway
+	if err = client.OpenGateway(context.TODO()); err != nil {
+		panic(err)
+	}
+
+	s := make(chan os.Signal, 1)
+	signal.Notify(s, syscall.SIGINT, syscall.SIGTERM)
+	<-s
+}
+
+func VoiceRoleAdjustment(joined bool, e *events.GenericGuildVoiceState) error {
+	userid := e.Member.User.ID
+	gid := e.VoiceState.GuildID
+
+	roles, err := e.Client().Rest().GetRoles(gid)
+
+	if err != nil {
+		return fmt.Errorf("failed to get the role: %w", err)
+	}
+
+	var voicerole discord.Role
+	for _, role := range roles {
+		if role.Name == "Voice" {
+			voicerole = role
+		}
+	}
+
+	if voicerole.ID == 0 {
+		return errors.New("failed to find the role \"Voice\"") 
+	}
+
+	if joined {
+		err = e.Client().Rest().AddMemberRole(gid, userid, voicerole.ID)
+	} else {
+		err = e.Client().Rest().RemoveMemberRole(gid, userid, voicerole.ID)
+	}
+	
+	if err != nil {
+		return fmt.Errorf("failed to adjust the role: %w", err)
+		
+	}
+
+	return nil
+}
